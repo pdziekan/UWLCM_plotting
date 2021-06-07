99f694e27dd8ce69bcffa13ee446a7a810ac4573
Piotr Dziekan <pdziekanp@gmail.com>
2021-06-07 18:53:35 +0200

    dycoms series wip

--- drawbicyc/include/drawbicyc/cases/Dycoms_RF02/plots.hpp
+++ drawbicyc/include/drawbicyc/cases/Dycoms_RF02/plots.hpp
@@ -15,7 +15,7 @@ const std::vector<std::string> series_dycoms({
 ////"mass_dry",
 // "acc_precip",
  "cl_nc",
- "cloud_base"
+ "cloud_base_dycoms"
 // ,"cl_gccn_meanr"
 // ,"cl_avg_cloud_rad"
 // ,"cl_gccn_conc", "gccn_conc"
