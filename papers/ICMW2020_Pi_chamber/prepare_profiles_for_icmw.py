2ff2213d538a93d16a88fee8202213f189e4dc87
Piotr Dziekan <pdziekanp@gmail.com>
2021-07-02 13:30:43 +0200

    pi chamber profiles preparation: p3 comp

--- papers/ICMW2020_Pi_chamber/prepare_profiles_for_icmw.py
+++ papers/ICMW2020_Pi_chamber/prepare_profiles_for_icmw.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from sys import argv
 
 ICMW_UWLCM_names = {
   "time" : "position",
@@ -7,19 +8,6 @@ ICMW_UWLCM_names = {
   "Qv" : "rv",
   "RH" : "RH"
 }
-
-# from simulations prepared on 29.10.2020
-#inj_rate_to_N = {
-#  "0.25e9" : "13",
-#  "0.5e9"  : "35",
-#  "0.75e9" : "60",
-#  "1.2e9"  : "120",
-#  "1.5e9"  : "180",
-#  "2.5e9"  : "400",
-#  "5e9"    : "1200",
-#  "6e9"    : "1800",
-#  "7.5e9"    : "2500"
-#}
 
 inj_rate_to_N = {
   "1e5" : "4",
@@ -35,7 +23,6 @@ inj_rate_SstpSrc = {
   "3e6" : "100",
 }
 
-
 def read_UWLCM_array(file_obj):
   arr_name = file_obj.readline()
   file_obj.readline() # discarded line with size of the array
@@ -43,7 +30,7 @@ def read_UWLCM_array(file_obj):
   line = line.split(" ")
   del line[0]
   del line[len(line)-1]
-  arr = map(float,line)
+  arr = [float(x) for x in line]
   return np.array(arr), arr_name
 
 def read_UWLCM_var(file_obj, var_name):
@@ -56,13 +43,18 @@ def read_UWLCM_var(file_obj, var_name):
 
 time_points = np.arange(0,5401,30)
 
+assert(len(argv)==2)
+
+data_dir = argv[1]
+
 for inj_rate in inj_rate_to_N:
   for var in ["T", "RH", "Qv"]:
-    out_file_name = "/scratch/plgdziekan/LES_N"+inj_rate_to_N[inj_rate]+"_2D_"+var
+    out_file_name = data_dir+"/LES_N"+inj_rate_to_N[inj_rate]+"_2D_"+var
     if os.path.exists(out_file_name):
       os.remove(out_file_name)
     out_file=open(out_file_name,'a')
     for time in time_points:
-      arr = read_UWLCM_var(open("/lu/topola/home/pdziekan/wyniki/Pi_chamber/rysy_copy/output_pichamber_3D_sgs_turbadve0_source"+inj_rate+"SD1@domainSstp"+inj_rate_SstpSrc[inj_rate]+"_SideRH80_Coal0_out_lgrngn_pi_chamber_icmw_profiles_"+str(time)+"_"+str(time)+".dat","r"), ICMW_UWLCM_names[var])
-      np.savetxt(out_file, arr, newline=" ", delimiter=" ", fmt="%1.5f")
+      file_name = data_dir+"/output_pichamber_3D_sgs_turbadve0_source"+inj_rate+"SD1@domainSstp"+inj_rate_SstpSrc[inj_rate]+"_SideRH80_Coal0_out_lgrngn_pi_chamber_icmw_profiles_"+str(time)+"_"+str(time)+".dat"
+      arr = read_UWLCM_var(open(file_name,"r"), ICMW_UWLCM_names[var])
+      np.savetxt(out_file, arr[:], newline=" ", delimiter=" ", fmt="%1.5f")
       out_file.write("\n")
