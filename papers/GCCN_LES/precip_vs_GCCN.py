e1062c3d300fc5b1edc3c2176b369b58352b3c16
pdziekan <pdziekanp@gmail.com>
2021-09-14 14:52:51 +0200

    precip vs gccn: precip over cloud cover, but differently at cloud base and at surface

--- papers/GCCN_LES/precip_vs_GCCN.py
+++ papers/GCCN_LES/precip_vs_GCCN.py
@@ -18,19 +18,22 @@ profs_from_it = int(sys.argv[3])
 profs_to_it = int(sys.argv[4])
 qlimit = float(sys.argv[5])
 relative = int(sys.argv[6])
+cloud_type = int(sys.argv[7]) # ==0 - stratocumulus, !=0 - cumulus
 
 #varlabels = ["{\it ScNc30}", "{\it ScNc40\_salt\_CCN}", "{\it ScNc45}", "{\it ScNc105}"]
 
-varlabels = ["{\it Cu38}", "{\it Cu60}", "{\it Cu88}"]
-CCN_conc = [52.5, 105, 210]
-nc = [38, 60, 85]
-ensemble = [5,6,4]
-
-
-#varlabels = ["{\it Sc38}", "{\it Sc59}", "{\it Sc115}"]
-#CCN_conc = [95, 190, 475]
-#nc = [38, 59, 115]
-#ensemble = [2,2,2]
+if cloud_type != 0:
+  varlabels = ["{\it Cu38}", "{\it Cu60}", "{\it Cu88}"]
+  CCN_conc = [52.5, 105, 210]
+  nc = [38, 60, 85]
+  ensemble = [5,6,4]
+  cl_cover_series_name = "cloud_cover_rico"
+else:
+  varlabels = ["{\it Sc38}", "{\it Sc59}", "{\it Sc115}"]
+  CCN_conc = [95, 190, 475]
+  nc = [38, 59, 115]
+  ensemble = [2,2,2]
+  cl_cover_series_name = "cloud_cover_dycoms"
 
 averaging_period = float(profs_to_it - profs_from_it) / 3600. # period over which series are averaged [h]; NOTE: we assume that series_from(to)_it = profs_from(to)_it / outfreq!
 
@@ -45,7 +48,7 @@ fig, axarr = plt.subplots(nploty, nplotx)#), constrained_layout=True )
 #prepare a list of output files, assuming the following order: prsitine, standard, polluted, for each 4 results: no GCCN, GCCN ,GCCNx5, GCCNx10
 series_file_names = []
 profs_file_names = []
-file_no = np.arange(7, len(sys.argv)-1 , 1)
+file_no = np.arange(8, len(sys.argv)-1 , 1)
 print file_no
 for no in file_no:
   series_file_names.append(sys.argv[no] + "series.dat")
@@ -63,8 +66,12 @@ for it in np.arange(12):
     mean_surf_precip = []
     tot_acc_surf_precip = []
     tot_acc_surf_precip_std_dev = []
+    tot_acc_surf_precip_over_cl_cover = []
+    tot_acc_surf_precip_over_cl_cover_std_dev = []
     prflux = []
     prflux_std_dev = []
+    prflux_over_cl_cover = []
+    prflux_over_cl_cover_std_dev = []
     prfluxDivByClFrac= []
     prfluxDivByClFrac_std_dev = []
     prfluxFromPrfluxVsClhght = []
@@ -85,6 +92,7 @@ for it in np.arange(12):
 
 # calc mean surf precip
   surf_precip = read_my_var(series_infile, "surf_precip")
+  surf_precip_std_dev = read_my_var(series_infile, "surf_precip_std_dev")
   mean_surf_precip.append(np.mean(surf_precip[series_from_it:series_to_it]))
 
 # calc acc surf precip
@@ -95,8 +103,22 @@ for it in np.arange(12):
     print "Could not find acc_precip_std_dev, setting to 0"
     acc_surf_precip_std_dev = np.zeros(len(acc_surf_precip))
 
+# read cloud cover
+  cl_cover = read_my_var(series_infile, cl_cover_series_name)
+  try:
+    cl_cover_std_dev = read_my_var(series_infile, cl_cover_series_name+"_std_dev")
+  except:
+    print "Could not find acc_precip_std_dev, setting to 0"
+    cl_cover_std_dev = np.zeros(len(cl_cover))
+
   tot_acc_surf_precip.append(acc_surf_precip[series_to_it] - acc_surf_precip[series_from_it])
   tot_acc_surf_precip_std_dev.append(acc_surf_precip_std_dev[series_to_it] + acc_surf_precip_std_dev[series_from_it])
+ 
+  surf_precip_over_cl_cover = surf_precip / cl_cover
+  surf_precip_over_cl_cover_std_dev = surf_precip / cl_cover * np.sqrt(np.power(surf_precip_std_dev / surf_precip,2) + np.power(cl_cover_std_dev / cl_cover,2))
+  tot_acc_surf_precip_over_cl_cover.append(np.mean(surf_precip_over_cl_cover[series_from_it:series_to_it]))
+  tot_acc_surf_precip_over_cl_cover_std_dev.append(np.mean(surf_precip_over_cl_cover_std_dev[series_from_it:series_to_it]))
+#  tot_acc_surf_precip_over_cl_cover_std_dev = []
 
 # find cloud base
   try:
@@ -122,6 +144,10 @@ for it in np.arange(12):
     print "Could not find the prflux profile, setting prflux at cloud base = 0"
     prflux.append(0)
     prflux_std_dev.append(0)
+# --- prflux at cloud base height divided by average cloud cover---
+  prflux_over_cl_cover.append(prflux[-1] / np.mean(cl_cover[series_from_it:series_to_it]))
+  prflux_over_cl_cover_std_dev.append(prflux_over_cl_cover[-1] * np.sqrt(np.power(prflux_std_dev[-1] / prflux[-1],2) + np.power(np.mean(cl_cover_std_dev[series_from_it:series_to_it]) / np.mean(cl_cover[series_from_it:series_to_it]),2)))
+
 # --- get prflux at cloud base from the prflux vs cloud height profile ---
   try:
     clb_prflux = read_my_var(profs_infile, "base_prflux_vs_clhght")
@@ -192,9 +218,11 @@ for it in np.arange(12):
     tot_acc_accr = [24. * 3600. * x for x in tot_acc_accr] # turn into g / m^3 / day
     tot_acc_accr_std_dev = [24. * 3600. * x for x in tot_acc_accr_std_dev] # same
     print "surf precip", tot_acc_surf_precip
+    print "surf precip divided by cloud cover", tot_acc_surf_precip_over_cl_cover
     print "prflux at cloud base altitude: ",prflux
     #print "prflux at cloud base altitude divided by cloud fraction: ",prfluxDivByClFrac
     print "prflux at cloud base from the prflux vs cloud height profile: ",prfluxFromPrfluxVsClhght
+    print "prflux at cloud base altitude divided by cloud cover: ",prflux_over_cl_cover
     print "acnv", tot_acc_acnv
     print "accr", tot_acc_accr
     #axarr[0].plot(GCCN_conc, mean_surf_precip, 'o')
@@ -206,10 +234,12 @@ for it in np.arange(12):
       axarr[1,0].errorbar(GCCN_CCN_rat, tot_acc_acnv - tot_acc_acnv[0]              , yerr = (tot_acc_acnv_std_dev + tot_acc_acnv_std_dev[0])               / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
       axarr[1,1].errorbar(GCCN_CCN_rat, tot_acc_accr - tot_acc_accr[0]              , yerr = (tot_acc_acnv_std_dev + tot_acc_acnv_std_dev[0])               / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
     else:
-      axarr[0,0].errorbar(GCCN_conc, tot_acc_surf_precip, yerr = tot_acc_surf_precip_std_dev / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.', label = varlabels[(it)/4])
-      axarr[0,1].errorbar(GCCN_conc, prflux,              yerr = prflux_std_dev              / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
-      axarr[1,0].errorbar(GCCN_conc, tot_acc_acnv,        yerr = tot_acc_acnv_std_dev        / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
-      axarr[1,1].errorbar(GCCN_conc, tot_acc_accr,        yerr = tot_acc_acnv_std_dev        / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
+#      axarr[0,0].errorbar(GCCN_conc, tot_acc_surf_precip,               yerr = tot_acc_surf_precip_std_dev / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.', label = varlabels[(it)/4])
+      axarr[0,0].errorbar(GCCN_conc, tot_acc_surf_precip_over_cl_cover, yerr = tot_acc_surf_precip_over_cl_cover_std_dev / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.', label = varlabels[(it)/4])
+#      axarr[0,1].errorbar(GCCN_conc, prflux,                            yerr = prflux_std_dev              / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
+      axarr[0,1].errorbar(GCCN_conc, prflux_over_cl_cover,              yerr = prflux_over_cl_cover_std_dev / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
+      axarr[1,0].errorbar(GCCN_conc, tot_acc_acnv,                      yerr = tot_acc_acnv_std_dev        / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
+      axarr[1,1].errorbar(GCCN_conc, tot_acc_accr,                      yerr = tot_acc_acnv_std_dev        / np.sqrt(ensemble[(it)/4]), marker='o', fmt='.')
 
 if(relative):
   axarr[0,0].set_ylabel('exc. surf. precip. [mm/day]')
